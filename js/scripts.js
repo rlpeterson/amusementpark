c89fc6ec1957498812442646a8dd5551ef99b342
Dan Burbach and Rob Peterson <dan@>
2018-09-17 20:12:44 -0700

    week 8 - added initial code. can now pop up correct results based off of height.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
-  var age = parseInt(prompt("How old are you?"));
+  var height = parseInt(prompt("How tall are you? (inches)"));
 
-  if (age > 18) {
-    $('#voting-age').show();
-  } else if (age === 18) {
-    alert("If you havent voted please follow the information on how to register.");
-    $('#voting-age').show();
-    $('#register').show();
+  if (height > 60) {
+    $('#above60-valid').show();
+  } else if (height === 60) {
+    alert("Congrats, you can ride all the rides!");
+    $('#above60').show();
+  } else if (height === 59) {
+    alert("How's the weather down there? Try these kiddie rides");
+    $('#below60-valid').show();
   } else {
-    $('#under-18').show();
+    $('#below60').show();
   }
 });
